0921c68420dc458e8f9fe84b56978a0201db03ef
Radha <radha@skylarkdrones.com>
2022-05-21 14:33:40 -0400

    refactor: Clean up code

--- Project/rbe500_project/scripts/rrp_pid.py
+++ Project/rbe500_project/scripts/rrp_pid.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 
 from math import pi
+from time import sleep
 from typing import List, Tuple
 
 import rospy
+from rrp_ik_client import get_rrp_ik
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
-
-from rrp_ik_client import get_rrp_ik
-
-# Poses
-pose1 = [0.0, 0.77, 0.34]
-pose2 = [-0.345, 0.425, 0.24]
-pose3 = [0.67, -0.245, 0.14]
-pose4 = [0.77, 0.0, 0.39]
-poses = [pose1, pose2, pose3, pose4]
 
 ROSPY_RATE = 150
 
@@ -25,17 +18,18 @@ class Controller:
             self,
             control_entity: str,
             p: float = 0.0,
-            d: float = 0.0,
             i: float = 0.0,
+            d: float = 0.0,
             set_point: float = 0.0,
             ):
         self.control_entity = control_entity
         self.Kp = p
-        self.Kd = d
         self.Ki = i
-        self.dt = 1/ROSPY_RATE
+        self.Kd = d
         self.set_point = set_point  # reference (desired value)
         self.previous_error = 0
+
+        self.dt = 1 / ROSPY_RATE
         self.P_term = 0.0
         self.I_term = 0.0
         self.D_term = 0.0
@@ -66,33 +60,34 @@ class Controller:
         self.Kd = d
 
 
-# Publish input commands to rrp
 class RRP_bot:
-    def __init__(self, positions: List[Tuple]):
+    def __init__(self, positions: List[Tuple]) -> None:
         rospy.init_node("rrp_bot_move")
         rospy.loginfo("Press Ctrl + C to terminate")
 
         self.poses = positions
         self.des_states = None
+        self.actual_states = []
 
-        self.jnt1_topic = "/rrp/joint1_effort_controller/command"
-        self.jnt2_topic = "/rrp/joint2_effort_controller/command"
-        self.jnt3_topic = "/rrp/joint3_effort_controller/command"
-
-        self.jnt_pub1 = rospy.Publisher(self.jnt1_topic, Float64, queue_size=10)
-        self.jnt_pub2 = rospy.Publisher(self.jnt2_topic, Float64, queue_size=10)
-        self.jnt_pub3 = rospy.Publisher(self.jnt3_topic, Float64, queue_size=10)
+        self.jnt_pub1 = rospy.Publisher(
+            "/rrp/joint1_effort_controller/command", Float64, queue_size=10
+        )
+        self.jnt_pub2 = rospy.Publisher(
+            "/rrp/joint2_effort_controller/command", Float64, queue_size=10
+        )
+        self.jnt_pub3 = rospy.Publisher(
+            "/rrp/joint3_effort_controller/command", Float64, queue_size=10
+        )
 
         self.pos_control3 = Controller("position")
         self.orient_control1 = Controller("orientation")
         self.orient_control2 = Controller("orientation")
 
-        self.actual_states = []
-        self.logging_counter = 0
-        self.trajectory = list()
-
         # Subscribe to joint states
         rospy.Subscriber("/rrp/joint_states", JointState, self.subs_callback)
+
+        self.logging_counter = 0
+        self.trajectory = list()
 
         try:
             self.run()
@@ -112,9 +107,7 @@ class RRP_bot:
 
     def control(self, kp1, ki1, kd1, kp2, ki2, kd2, kp3, ki3, kd3):
 
-        reached_1 = False
-        reached_2 = False
-        reached_3 = False
+        reached_1, reached_2, reached_3 = False, False, False
 
         self.orient_control1.set_pid(kp1, ki1, kd1)
         self.orient_control2.set_pid(kp2, ki2, kd2)
@@ -172,20 +165,20 @@ class RRP_bot:
         return True if error < tol else False
 
     def run(self):
-        from time import sleep
-        sleep(5)
+        sleep(3)
         for pose in self.poses:
             x1, y1, z1 = pose
 
-            # Get desired joint states
             self.des_states = get_rrp_ik(x1, y1, z1)
-
             self.control(0.25, 0.02, 0.45, 0.25, 0.0125, 0.1, 0.25, 0.01, 0.1)
 
 
 if __name__ == "__main__":
-    RRP_bot(
-        [(0.0, 0.77, 0.34), (-0.345, 0.425, 0.24)]
-    )
+    RRP_bot([
+        (0.0, 0.77, 0.34),
+        (-0.345, 0.425, 0.24),
+        (0.67, -0.245, 0.14),
+        (0.77, 0.0, 0.39)
+    ])
     
 
